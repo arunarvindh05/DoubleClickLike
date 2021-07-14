68a9f894230d7066cded342847132c29db740795
arunarvindh05 <arunarvindh05@gmail.com>
2021-07-14 20:39:44 +0530

    fix for changes

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const image = document.querySelector('.img_post');
 
 likes.innerText = "No likes";
 let count = 0;
-
 image.addEventListener('dblclick', () => {
     count++; //likes count
     if (count === 1) {
